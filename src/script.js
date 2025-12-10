2e9eb18bffeea7b1b2efd6f1c5885e2699254d76
khushpatel00 <patelxkhush@gmail.com>
2025-12-10 16:54:19 +0530

    jitter reduced, custom scrollbar

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 gsap.set(".bannerred", { perspective: 800 });
 gsap.set(".perspective-animation", { display: 'block' });
+gsap.set("img", { display: 'block' });
+gsap.set(".switch", { display: 'block' });
 
 gsap.from('.bannerred', {
     y: -20,
@@ -10,7 +12,8 @@ gsap.from('.bannerred', {
 gsap.from('img', {
     top: '50%', 
     left: '50%',
-    opacity: 0,
+    opacity: -0.1,
+    scale: 0.8,
     // duration: 0.5,
     scrollTrigger: {
         trigger: '.bannerred',
@@ -22,6 +25,12 @@ gsap.from('img', {
     }
 
 })
+
+gsap.from('.switch', {
+    y: -100,
+    delay: 0.7,
+})
+
 gsap.from('h1, h2',{
     y: -20,
     duration: 0.3,
